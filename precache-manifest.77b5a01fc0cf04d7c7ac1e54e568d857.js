562ce98eab1d37d02b5bab26dc2907360d8c8d02
ChironJon <immrwhiskers4@gmail.com>
2020-01-28 19:39:52 -0600

    Updates

---
+++ precache-manifest.77b5a01fc0cf04d7c7ac1e54e568d857.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200377080f7241a3cfe89ba15c604738",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "25165a93389fb0113ca7",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a8110b2d7b123d352ca9",
+    "url": "/robofriends/static/css/main.63998a4a.chunk.css"
+  },
+  {
+    "revision": "25165a93389fb0113ca7",
+    "url": "/robofriends/static/js/2.7136d570.chunk.js"
+  },
+  {
+    "revision": "a8110b2d7b123d352ca9",
+    "url": "/robofriends/static/js/main.b47d5cf4.chunk.js"
+  },
+  {
+    "revision": "5645dfe01f57d087ff9e",
+    "url": "/robofriends/static/js/runtime-main.47a7d18e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
